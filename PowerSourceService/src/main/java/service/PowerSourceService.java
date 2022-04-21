114a8fa497a8bff0df2a16b38b3227db9a5167a5
Binura <binuranavinda2000@gmail.com>
2022-04-21 22:23:00 +0530

    Power Source Service Implementation included

---
+++ PowerSourceService/src/main/java/service/PowerSourceService.java
@@ -0,0 +1,80 @@
+package service;
+
+import model.PowerSource;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+
+@Path("/PowerSource")
+public class PowerSourceService {
+	PowerSource ps = new PowerSource();
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertItem(@FormParam("Name") String Name,
+								@FormParam("Address") String Address,
+								@FormParam("Province") String Province,
+								@FormParam("Type") String Type,
+								@FormParam("PowerGenerated") String PowerGenerated,
+								@FormParam("Maintenance_Day") String Maintenance_Day,
+								@FormParam("Head_Engineer") String Head_Engineer)
+	{
+		String output = ps.insertPowerSource(Name,Address,Province,Type,PowerGenerated,Maintenance_Day,Head_Engineer);
+		return output;
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readPowerSource() {
+		return ps.readPowerSource();
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateItem(String itemData) {
+		//Covert the input string to a JSON object
+		JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+		
+		//Read the values from the JSON Object
+		String ID = itemObject.get("ID").getAsString();
+		String Name = itemObject.get("Name").getAsString();
+		String Address = itemObject.get("Address").getAsString();
+		String Province = itemObject.get("Province").getAsString();
+		String Type = itemObject.get("Type").getAsString();
+		String PowerGenerated = itemObject.get("PowerGenerated").getAsString();
+		String Maintenance_Day = itemObject.get("Maintenance_Day").getAsString();
+		String Head_Engineer = itemObject.get("Head_Engineer").getAsString();
+		
+		String output = ps.updatePowerSource(ID,Name,Address,Province,Type,PowerGenerated,Maintenance_Day,Head_Engineer);
+		return output;
+	}
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deletePowerSource(String powerSourceData) {
+		//Convert the input string to an XML document
+		Document doc = Jsoup.parse(powerSourceData,"",Parser.xmlParser());
+		
+		//Read the value from the element <itemID>
+		String ID = doc.select("ID").text();
+		String output = ps.deletePowerSource(ID);
+		return output;
+	}
+}
